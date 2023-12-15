2c1b49d1fa9cc369a286516c1b5fcada2c155503
Sylvia Achieng <135863767+SylviaAchieng@users.noreply.github.com>
2023-12-15 20:23:26 +0300

    Add files via upload

---
+++ mystack.py
@@ -0,0 +1,10 @@
+stack=[]
+stack.append("a")
+stack.append("b")
+stack.append("c")
+print("initial stack", stack)
+print("elements poped from stack:")
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print("stack after elements are popped:", stack)
