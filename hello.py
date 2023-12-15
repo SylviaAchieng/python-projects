a5eb983f49d31b7b588a551cfc6a7c4d9577f610
Sylvia Achieng <135863767+SylviaAchieng@users.noreply.github.com>
2023-12-15 20:04:05 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,20 @@
+name="Sylvia, Achieng"
+
+print(name)
+print(len(name))  #len method gives the length of a string
+
+#string methods
+print(name.capitalize()) #capitalizes the first letter of a string
+
+print(name.upper())  #covert the whole str to uppercase
+
+print(name.lower())    #converts str to lowercase
+
+print(name.replace("S", "C"))   #replace a str with another str
+
+print(name.split(","))   #splits the string into substrings if it finds instances of the separator
+
+
+#naming a variable using camel casing means the first letter is small and the next words are capitalised
+daysInMonth=30
+print("there are", daysInMonth, "days in a month")
