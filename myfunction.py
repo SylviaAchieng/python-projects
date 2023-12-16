3c22e992ac2e6a51856191e8513837695cb431b4
Sylvia Achieng <135863767+SylviaAchieng@users.noreply.github.com>
2023-12-16 22:24:39 +0300

    Add files via upload

---
+++ myfunction.py
@@ -0,0 +1,25 @@
+#declaring a function
+
+def myfunc(name):   #name is the parameter
+    print("hello " + name)
+
+#calling a function
+myfunc("sylvia")    #sylvia argument
+
+#you can call a function more than one time
+myfunc("cynthia")
+myfunc("john")
+
+
+#passing list as a parameter
+
+'''def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
+
+prints : apple banana cherry
+'''
