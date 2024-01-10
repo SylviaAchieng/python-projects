b552c2feff4df88150aed87d69a93e8b0da28304
Sylvia Achieng <135863767+SylviaAchieng@users.noreply.github.com>
2024-01-10 11:37:07 +0300

    Add files via upload

---
+++ stringformatting.py
@@ -0,0 +1,4 @@
+#String formatting using f-strings (formatted string literals) allows you to embed expressions inside string literals
+name = "Jane"
+age = 25
+print(f"My name is {name} and I am {age} years old.")
