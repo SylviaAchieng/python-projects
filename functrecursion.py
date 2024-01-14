52dba607f39b8451c15aceb3c060029ef6835c76
Sylvia Achieng <135863767+SylviaAchieng@users.noreply.github.com>
2024-01-14 18:56:00 +0300

    Add files via upload

---
+++ functrecursion.py
@@ -0,0 +1,12 @@
+# function recursion means a defined function can call itself.
+
+def tri_recursion(k):
+  if(k>0):
+    result = k+tri_recursion(k-1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
